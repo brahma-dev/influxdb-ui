9e1c34e485a4beedffc4824b331479eb5067923a
Brahma Dev <me@brahma.world>
2019-07-22 10:14:32 +0530

    Show value of boolean fields

--- src/components/QuerySeriesDataRow.js
+++ src/components/QuerySeriesDataRow.js
@@ -2,13 +2,19 @@
 import React, { Component } from 'react';
 
 class QuerySeriesDataRow extends Component {  
-
+  renderItem(item){
+    if(typeof item == "boolean"){
+      return item===true?"true":"false"
+    }else{
+      return item
+    }
+  }
   render() {
     //  Display data row:
     return (
         <tr>
             {this.props.datarow.map(function(item, index) {
-                return <td key={index}>{item}</td>;
+                return <td key={index}>{this.renderItem(item)}</td>;
             })}
         </tr>
     );
